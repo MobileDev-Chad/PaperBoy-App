db96b72b79430921a2dbdc0332bbfcfeaad19e97
MobileDev-Chad <chadwin.allotey@gmail.com>
2022-12-21 22:29:23 -0500

    corrected file name

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,9 +1,9 @@
-export const back = require("../assets/icons/back_icon.png");
+export const back = require("../assets/icons/back.png");
 export const cart = require("../assets/icons/cart.png");
 export const email = require("../assets/icons/email.png");
 export const home = require("../assets/icons/home.png");
 export const magnify = require("../assets/icons/magnify.png");
-export const menu = require("../assets/icons/menu_icon.png");
+export const menu = require("../assets/icons/bar_menu.png");
 
 export default {
     back,
