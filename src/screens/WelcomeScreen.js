cbff2626b93f9b35be0cee0aa91e9720d6fcca2b
MobileDev-Chad <chadwin.allotey@gmail.com>
2022-12-23 16:52:48 -0500

    Added WelcomeScreen image

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import Screen from '../components/Screen'
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { COLORS, images } from "../constants";
+import Screen from "../components/Screen";
 
 const WelcomeScreen = () => {
+  // Render
+
   return (
     <Screen>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Image
+          source={images.invincible}
+          resizeMode="contain"
+          style={{ width: "75%", height: "75%" }}
+        />
+      </View>
 
-        <Text>WelcomeScreen</Text>
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      ></View>
     </Screen>
-  )
-}
+  );
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
