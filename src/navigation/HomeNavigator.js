b75fa71ffe36443309223aa5fe8431e4eb09c9d4
MobileDev-Chad <chadwin.allotey@gmail.com>
2022-12-24 14:28:39 -0500

    Create HomeNavigator.js

---
+++ src/navigation/HomeNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen, DetailsScreen } from "../screens";
+import { COLORS, icons } from "../constants";
+
+const Stack = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Stack.Navigator presentaion="modal" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Home" component={HomeScreen} />
+
+    <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+  </Stack.Navigator>
+);
+
+export default HomeNavigator;
