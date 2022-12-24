b519de24a0033bf6e58cabf0f9090651af5cbf64
MobileDev-Chad <chadwin.allotey@gmail.com>
2022-12-23 19:40:32 -0500

    Create storage.js

---
+++ src/auth/storage.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeKey = '@storage_Key'
+const storeData = async (value) => {
+  try {
+    await AsyncStorage.setItem(storeKey, value)
+  } catch (e) {
+    // saving error
+  }
+}
+
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem(storeKey)
+    if(value !== null) {
+      // value previously stored
+    }
+  } catch(e) {
+    // error reading value
+  }
+}
