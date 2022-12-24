cacd479194cabdf536f2fce972f3a9bcd21b2c91
MobileDev-Chad <chadwin.allotey@gmail.com>
2022-12-23 19:40:52 -0500

    Update WelcomeScreen.js

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { COLORS, images } from "../constants";
+import { COLORS, images, SIZES, FONTS } from "../constants";
 import Screen from "../components/Screen";
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ navigation }) => {
   // Render
 
   return (
@@ -17,11 +17,62 @@ const WelcomeScreen = () => {
         />
       </View>
 
-      <View
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      ></View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={{ alignItems: "center", marginHorizontal: SIZES.padding }}>
+          <Text style={{ ...FONTS.h2 }}>PosterBoy</Text>
+          <Text
+            style={{
+              color: COLORS.gray,
+              marginTop: SIZES.padding,
+              textAlign: "center",
+              ...FONTS.body3,
+            }}
+          >
+            Get your favorite Smash Bros. characters delivered right to your
+            doorstep!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          style={[styles.shadow,{
+            marginTop: SIZES.padding * 2,
+            width: "70%",
+            height: 50,
+            alignItems: "center",
+            justifyContent: "center",
+          }]}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <LinearGradient
+            style={{
+              height: "100%",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 15,
+            }}
+            colors={["#46aeff", "#5884ff"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Shop!</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create ({
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  }
+})
 
 export default WelcomeScreen;
