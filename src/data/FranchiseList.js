06157c162d0dc16dd8c6017bac67ed70aa21bf71
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-01 21:17:16 -0500

    Added linear gradient colors

--- src/data/FranchiseList.js
+++ src/data/FranchiseList.js
@@ -1,253 +1,293 @@
 export const FranchiseList = [
   {
     id: "1",
-    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1669684728/Smash%20Characters/800px-Sora-Alt_3_SSBU_ehryq0.png",
     franchise: "Super Mario",
   },
   {
     id: "2",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501875/Series/donkey-kong_tppruc.svg",
     franchise: "Donkey Kong",
   },
   {
     id: "3",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1669681201/Smash%20Characters/11_-_fcCLhoC_dietap.png",
     franchise: "Zelda",
   },
   {
     id: "4",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595194655/Series/fxdmwjgnygjbaetfvjzy.svg",
     franchise: "Metroid",
   },
 
   {
     id: "5",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595197033/Series/murkcxf7ykhhz3muwo7y.svg",
     franchise: "Yoshi",
   },
   {
     id: "6",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198280/Series/bilh5ggeojt5hxozb8fp.svg",
     franchise: "Kirby",
   },
   {
     id: "7",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198574/Series/g6sv3naruucu8qs0vv3u.svg",
     franchise: "Star Fox",
   },
   {
     id: "8",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
     franchise: "Pokemon",
   },
 
   {
     id: "10",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595199211/Series/lpuxxedjrwzwgnvuklt0.svg",
     franchise: "EarthBound",
   },
   {
     id: "11",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595199363/Series/rt7xqwarfhrhlkqzlf1t.svg",
     franchise: "F-Zero",
   },
 
   {
     id: "15",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595211554/Series/elcz77vgrgasu9l1s5hr.svg",
     franchise: "Ice Climbers",
   },
 
   {
     id: "21",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
     franchise: "Fire Emblem",
   },
-  {
-    id: "021",
-    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
-    franchise: "Fire Emblem",
-  },
-
   {
     id: "26",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595214130/Series/gr73dvd2a3sfx7q4gujm.svg",
     franchise: "Game & Watch",
   },
 
   {
     id: "28",
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595263748/Series/avacpo60oulifezowbm3.svg",
     franchise: "Kid Icarus",
   },
 
   {
     id: "30",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595264211/Series/d2w452qfebtvfkrpyrzj.svg",
     franchise: "Wario Ware",
   },
   {
     id: "31",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595264393/Series/bhoweuqpf6xchrfo8kwl.svg",
     franchise: "Metal Gear",
   },
 
   {
     id: "38",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265319/Series/ouolfmapifqlj13smqpn.svg",
     franchise: "Sonic",
   },
 
   {
     id: "40",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265648/Series/lqi9s9rpx8zerubf4xb8.svg",
     franchise: "Pikmin",
   },
 
   {
     id: "42",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265939/Series/jkh9sgvqmtpaeahhdurk.svg",
     franchise: "R.O.B.",
   },
 
   {
     id: "45",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595550229/Series/x0xuomhx7l56zuz34q1j.svg",
     franchise: "Animal Crossing",
   },
   {
     id: "46",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595550778/Series/menz9dyv8cama8xtgn2t.svg",
     franchise: "Mega Man",
   },
   {
     id: "47",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595551025/Series/p0t4eg7dznvxwbzunp8p.svg",
     franchise: "Wii Fit",
   },
 
   {
     id: "49",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595551548/Series/jeeijhihz95liemloxpm.svg",
     franchise: "Punch-Out!!",
   },
 
   {
     id: "51",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552332/Series/ofc2xcyawclffowvoqu1.svg",
     franchise: "Ultimate",
   },
 
   {
     id: "55",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552783/Series/y1lryzos8jirj8yjgexq.svg",
     franchise: "Pac-Man",
   },
 
   {
     id: "57",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553030/Series/a1pc0xxngopvv6i8u60k.svg",
     franchise: "Xenoblade",
   },
 
   {
     id: "59",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553304/Series/qtvpmemuwitz4mdo0vuv.svg",
     franchise: "Duck Hunt",
   },
   {
     id: "60",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553458/Series/styqpzn2nur6otldctue.svg",
     franchise: "Street Fighter",
   },
 
   {
     id: "61",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553876/Series/rsyt1blkqi8raf6uhjky.svg",
     franchise: "Final Fantasy",
   },
   {
     id: "63",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595554138/Series/tuovz4rsz9rb3h4ixdsg.svg",
     franchise: "Bayonetta",
   },
-
-  {
-    id: "66",
-
-    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595698474/Series/yenuncg5oknxmowsq3hh.svg",
-    franchise: "Castlevania",
-  },
   {
     id: "066",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595698474/Series/yenuncg5oknxmowsq3hh.svg",
     franchise: "Castlevania",
   },
 
   {
     id: "71",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699295/Series/eyindgcmwk93heqhqt5u.svg",
     franchise: "Persona",
   },
   {
     id: "72",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699474/Series/qbsfx54saqnvlxpoqsp1.svg",
     franchise: "Dragon Quest",
   },
   {
     id: "73",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699735/Series/bn619ul35qgaoec7pmmp.svg",
     franchise: "Banjo-Kazooie",
   },
   {
     id: "74",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699847/Series/tvccohorx22fmsbebnsx.svg",
     franchise: "Fatal Fury",
   },
 
   {
     id: "76",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595700128/Series/qnwoxcjwqlxxt8sxwfbj.svg",
     franchise: "ARMS",
   },
   {
     id: "77",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635344820/Steve/MinecraftSymbol_bzvnuh.svg",
     franchise: "MINECRAFT",
   },
 
   {
     id: "81",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635345280/Kazuya/TekkenSymbol_urnhuy.svg",
     franchise: "Tekken",
   },
   {
     id: "82",
-
+    background_one: '#232526',
+    background_two: '#414345',
     icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635345332/Sora/KingdomHeartsSymbol_kljquu.svg",
     franchise: "Kingdom Hearts",
   },
