02c69df6ffa03c4b8609f01642c410fd1176ec9b
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-01 19:50:04 -0500

    Create FranchiseList.js

---
+++ src/data/FranchiseList.js
@@ -0,0 +1,480 @@
+export const FranchiseList = [
+  {
+    id: "1",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "2",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501875/Series/donkey-kong_tppruc.svg",
+    franchise: "Donkey Kong",
+  },
+  {
+    id: "3",
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1669681201/Smash%20Characters/11_-_fcCLhoC_dietap.png",
+    franchise: "Zelda",
+  },
+  {
+    id: "4",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595194655/Series/fxdmwjgnygjbaetfvjzy.svg",
+    franchise: "Metroid",
+  },
+  {
+    id: "04",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595194655/Series/fxdmwjgnygjbaetfvjzy.svg",
+    franchise: "Metroid",
+  },
+  {
+    id: "5",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595197033/Series/murkcxf7ykhhz3muwo7y.svg",
+    franchise: "Yoshi",
+  },
+  {
+    id: "6",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198280/Series/bilh5ggeojt5hxozb8fp.svg",
+    franchise: "Kirby",
+  },
+  {
+    id: "7",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198574/Series/g6sv3naruucu8qs0vv3u.svg",
+    franchise: "Star Fox",
+  },
+  {
+    id: "8",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "9",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "10",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595199211/Series/lpuxxedjrwzwgnvuklt0.svg",
+    franchise: "EarthBound",
+  },
+  {
+    id: "11",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595199363/Series/rt7xqwarfhrhlkqzlf1t.svg",
+    franchise: "F-Zero",
+  },
+  {
+    id: "12",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "13",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "013",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "14",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "15",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595211554/Series/elcz77vgrgasu9l1s5hr.svg",
+    franchise: "Ice Climbers",
+  },
+  {
+    id: "16",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501930/Series/the-legend-of-zelda_naf34b.svg",
+    franchise: "Zelda",
+  },
+  {
+    id: "17",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501930/Series/the-legend-of-zelda_naf34b.svg",
+    franchise: "Zelda",
+  },
+  {
+    id: "18",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "19",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "20",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198574/Series/g6sv3naruucu8qs0vv3u.svg",
+    franchise: "Star Fox",
+  },
+  {
+    id: "21",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "021",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "22",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501930/Series/the-legend-of-zelda_naf34b.svg",
+    franchise: "Zelda",
+  },
+  {
+    id: "23",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501930/Series/the-legend-of-zelda_naf34b.svg",
+    franchise: "Zelda",
+  },
+  {
+    id: "24",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "25",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "025",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "26",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595214130/Series/gr73dvd2a3sfx7q4gujm.svg",
+    franchise: "Game & Watch",
+  },
+  {
+    id: "27",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198280/Series/bilh5ggeojt5hxozb8fp.svg",
+    franchise: "Kirby",
+  },
+  {
+    id: "28",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595263748/Series/avacpo60oulifezowbm3.svg",
+    franchise: "Kid Icarus",
+  },
+  {
+    id: "028",
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595263748/Series/avacpo60oulifezowbm3.svg",
+    franchise: "Kid Icarus",
+  },
+  {
+    id: "29",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595194655/Series/fxdmwjgnygjbaetfvjzy.svg",
+    franchise: "Metroid",
+  },
+  {
+    id: "30",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595264211/Series/d2w452qfebtvfkrpyrzj.svg",
+    franchise: "Wario Ware",
+  },
+  {
+    id: "31",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595264393/Series/bhoweuqpf6xchrfo8kwl.svg",
+    franchise: "Metal Gear",
+  },
+  {
+    id: "32",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "33-35",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "36",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501875/Series/donkey-kong_tppruc.svg",
+    franchise: "Donkey Kong",
+  },
+  {
+    id: "37",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595199211/Series/lpuxxedjrwzwgnvuklt0.svg",
+    franchise: "EarthBound",
+  },
+  {
+    id: "38",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265319/Series/ouolfmapifqlj13smqpn.svg",
+    franchise: "Sonic",
+  },
+  {
+    id: "39",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198280/Series/bilh5ggeojt5hxozb8fp.svg",
+    franchise: "Kirby",
+  },
+  {
+    id: "40",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265648/Series/lqi9s9rpx8zerubf4xb8.svg",
+    franchise: "Pikmin",
+  },
+  {
+    id: "41",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "42",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595265939/Series/jkh9sgvqmtpaeahhdurk.svg",
+    franchise: "R.O.B.",
+  },
+  {
+    id: "43",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501930/Series/the-legend-of-zelda_naf34b.svg",
+    franchise: "Zelda",
+  },
+  {
+    id: "44",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198574/Series/g6sv3naruucu8qs0vv3u.svg",
+    franchise: "Star Fox",
+  },
+  {
+    id: "45",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595550229/Series/x0xuomhx7l56zuz34q1j.svg",
+    franchise: "Animal Crossing",
+  },
+  {
+    id: "46",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595550778/Series/menz9dyv8cama8xtgn2t.svg",
+    franchise: "Mega Man",
+  },
+  {
+    id: "47",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595551025/Series/p0t4eg7dznvxwbzunp8p.svg",
+    franchise: "Wii Fit",
+  },
+  {
+    id: "48",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "49",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595551548/Series/jeeijhihz95liemloxpm.svg",
+    franchise: "Punch-Out!!",
+  },
+  {
+    id: "50",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "51",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552332/Series/ofc2xcyawclffowvoqu1.svg",
+    franchise: "Ultimate",
+  },
+  {
+    id: "52",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552332/Series/ofc2xcyawclffowvoqu1.svg",
+    franchise: "Ultimate",
+  },
+  {
+    id: "53",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552332/Series/ofc2xcyawclffowvoqu1.svg",
+    franchise: "Ultimate",
+  },
+  {
+    id: "54",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595263748/Series/avacpo60oulifezowbm3.svg",
+    franchise: "Kid Icarus",
+  },
+  {
+    id: "55",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595552783/Series/y1lryzos8jirj8yjgexq.svg",
+    franchise: "Pac-Man",
+  },
+  {
+    id: "56",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "57",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553030/Series/a1pc0xxngopvv6i8u60k.svg",
+    franchise: "Xenoblade",
+  },
+  {
+    id: "58",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "59",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553304/Series/qtvpmemuwitz4mdo0vuv.svg",
+    franchise: "Duck Hunt",
+  },
+  {
+    id: "60",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553458/Series/styqpzn2nur6otldctue.svg",
+    franchise: "Street Fighter",
+  },
+  {
+    id: "060",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553458/Series/styqpzn2nur6otldctue.svg",
+    franchise: "Street Fighter",
+  },
+  {
+    id: "61",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553876/Series/rsyt1blkqi8raf6uhjky.svg",
+    franchise: "Final Fantasy",
+  },
+  {
+    id: "63",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595554138/Series/tuovz4rsz9rb3h4ixdsg.svg",
+    franchise: "Bayonetta",
+  },
+  {
+    id: "65",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595194655/Series/fxdmwjgnygjbaetfvjzy.svg",
+    franchise: "Metroid",
+  },
+  {
+    id: "66",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595698474/Series/yenuncg5oknxmowsq3hh.svg",
+    franchise: "Castlevania",
+  },
+  {
+    id: "066",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595698474/Series/yenuncg5oknxmowsq3hh.svg",
+    franchise: "Castlevania",
+  },
+  {
+    id: "67",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501875/Series/donkey-kong_tppruc.svg",
+    franchise: "Donkey Kong",
+  },
+  {
+    id: "68",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595550229/Series/x0xuomhx7l56zuz34q1j.svg",
+    franchise: "Animal Crossing",
+  },
+  {
+    id: "69",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595198731/Series/mstlaeic4u1lgbzyfjdw.svg",
+    franchise: "Pokemon",
+  },
+  {
+    id: "70",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1594501902/Series/mario_ncgezx.svg",
+    franchise: "Super Mario",
+  },
+  {
+    id: "71",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699295/Series/eyindgcmwk93heqhqt5u.svg",
+    franchise: "Persona",
+  },
+  {
+    id: "72",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699474/Series/qbsfx54saqnvlxpoqsp1.svg",
+    franchise: "Dragon Quest",
+  },
+  {
+    id: "73",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699735/Series/bn619ul35qgaoec7pmmp.svg",
+    franchise: "Banjo-Kazooie",
+  },
+  {
+    id: "74",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595699847/Series/tvccohorx22fmsbebnsx.svg",
+    franchise: "Fatal Fury",
+  },
+  {
+    id: "75",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595213106/Series/uj3kecbitighgwf6khrd.svg",
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "76",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595700128/Series/qnwoxcjwqlxxt8sxwfbj.svg",
+    franchise: "ARMS",
+  },
+  {
+    id: "77",
+
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635344820/Steve/MinecraftSymbol_bzvnuh.svg",
+    franchise: "MINECRAFT",
+  },
+  {
+    id: "78",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553876/Series/rsyt1blkqi8raf6uhjky.svg",
+    franchise: "Final Fantasy",
+  },
+  {
+    id: "79",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553030/Series/a1pc0xxngopvv6i8u60k.svg",
+    franchise: "Xenoblade",
+  },
+  {
+    id: "80",
+
+    icon: "https://res.cloudinary.com/dfen7mkm8/image/upload/v1595553030/Series/a1pc0xxngopvv6i8u60k.svg",
+    franchise: "Xenoblade",
+  },
+  {
+    id: "81",
+
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635345280/Kazuya/TekkenSymbol_urnhuy.svg",
+    franchise: "Tekken",
+  },
+  {
+    id: "82",
+
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1635345332/Sora/KingdomHeartsSymbol_kljquu.svg",
+    franchise: "Kingdom Hearts",
+  },
+];
+export default FranchiseList;
