c361006e9bc3ac95c5106d20446971b9ce61891b
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-03 00:03:04 -0500

    Added images to local path
    This reverts commit c3fbdce37991ddd60d830e879eceb6a6e6a3fe96.

--- src/data/FranchiseList.js
+++ src/data/FranchiseList.js
@@ -1,383 +1,294 @@
-import { Asset } from "expo-asset";
-
 export const FranchiseList = [
   {
     id: "1",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/MarioSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672626215/icons%20smash/MarioSymbol_emooat.png",
     franchise: "Super Mario",
   },
   {
     id: "2",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/DKSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672626783/icons%20smash/DKSymbol_ksjilu.svg",
     franchise: "Donkey Kong",
   },
   {
     id: "3",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/ZeldaSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628210/icons%20smash/ZeldaSymbol_zhsmey.png",
     franchise: "Zelda",
   },
   {
     id: "4",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/MetroidSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628322/icons%20smash/MetroidSymbol_dixqaz.png",
     franchise: "Metroid",
   },
 
   {
     id: "5",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/YoshiSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628385/icons%20smash/YoshiSymbol_dsbnyb.png",
     franchise: "Yoshi",
   },
   {
     id: "6",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/KirbySymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628436/icons%20smash/KirbySymbol_lvgvxo.png",
     franchise: "Kirby",
   },
   {
     id: "7",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/StarFoxSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628498/icons%20smash/StarFoxSymbol_uwu8fe.png",
     franchise: "Star Fox",
   },
   {
     id: "8",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/PokemonSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628544/icons%20smash/PokemonSymbol_cut1mz.png",
     franchise: "Pokemon",
   },
 
   {
     id: "10",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/EarthboundSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628597/icons%20smash/EarthboundSymbol_ffy0ha.png",
     franchise: "EarthBound",
   },
   {
     id: "11",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/FZeroSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628646/icons%20smash/FZeroSymbol_qxc9hu.png",
     franchise: "F-Zero",
   },
 
   {
     id: "15",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/IceClimberSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628684/icons%20smash/IceClimberSymbol_xwd5r8.png",
     franchise: "Ice Climbers",
   },
 
   {
     id: "21",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/FireEmblemSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628732/icons%20smash/FireEmblemSymbol_tlb6fo.png",
     franchise: "Fire Emblem",
   },
   {
     id: "26",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/GameWatchSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628781/icons%20smash/Game_WatchSymbol_yyqnj2.png",
     franchise: "Game & Watch",
   },
 
   {
     id: "28",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/KidIcarusSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628833/icons%20smash/KidIcarusSymbol_lfyrhw.png",
     franchise: "Kid Icarus",
   },
 
   {
     id: "30",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/WarioSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628865/icons%20smash/WarioSymbol_hmpfo6.png",
     franchise: "Wario Ware",
   },
   {
     id: "31",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/MetalGearSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628915/icons%20smash/MetalGearSymbol_pyxngi.png",
     franchise: "Metal Gear",
   },
 
   {
     id: "38",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/SonicSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628967/icons%20smash/SonicSymbol_coqnce.png",
     franchise: "Sonic",
   },
 
   {
     id: "40",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/PikminSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672629001/icons%20smash/PikminSymbol_ald1jj.png",
     franchise: "Pikmin",
   },
 
   {
     id: "42",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/ROBSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/ROBSymbol_yqbye3.png",
     franchise: "R.O.B.",
   },
 
   {
     id: "45",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/AnimalCrossingSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/AnimalCrossingSymbol_mjlpov.png",
     franchise: "Animal Crossing",
   },
   {
     id: "46",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/MegaManSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/MegaManSymbol_un5wsn.png",
     franchise: "Mega Man",
   },
   {
     id: "47",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/WiiFitSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/WiiFitSymbol_agbohj.png",
     franchise: "Wii Fit",
   },
 
   {
     id: "49",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/PunchOutSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/PunchOutSymbol_m1ly9s.png",
     franchise: "Punch-Out!!",
   },
 
   {
     id: "51",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/SmashBrosSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/SmashBrosSymbol_qt6yta.png",
     franchise: "Ultimate",
   },
 
   {
     id: "55",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/PacManSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/PacManSymbol_kmk1cq.png",
     franchise: "Pac-Man",
   },
 
   {
     id: "57",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/XenobladeSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/XenobladeSymbol_pmj50m.png",
     franchise: "Xenoblade",
   },
 
   {
     id: "59",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/DuckHuntSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/DuckHuntSymbol_pwvtni.png",
     franchise: "Duck Hunt",
   },
   {
     id: "60",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/StreetFighterSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/StreetFighterSymbol_pwaziw.png",
     franchise: "Street Fighter",
   },
 
   {
     id: "61",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/FinalFantasySymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/FinalFantasySymbol_arsjpo.png",
     franchise: "Final Fantasy",
   },
   {
     id: "63",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/BayonettaSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/BayonettaSymbol_yeba5h.png",
     franchise: "Bayonetta",
   },
   {
-    id: "63",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/SplatoonSymbol.png")
-    ).uri,
-    franchise: "Splatoon",
-  },
-  {
     id: "066",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/CastlevaniaSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628123/icons%20smash/CastlevaniaSymbol_sxwbdh.png",
     franchise: "Castlevania",
   },
 
   {
     id: "71",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/PersonaSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/PersonaSymbol_d3qsgn.png",
     franchise: "Persona",
   },
   {
     id: "72",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/DragonQuestSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/DragonQuestSymbol_wfmntu.png",
     franchise: "Dragon Quest",
   },
   {
     id: "73",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/BanjoKazooieSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672629216/icons%20smash/BanjoKazooieSymbol_bejtrk.png",
     franchise: "Banjo-Kazooie",
   },
   {
     id: "74",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/FatalFurySymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/FatalFurySymbol_zd7inu.png",
     franchise: "Fatal Fury",
   },
 
   {
     id: "76",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/ARMSSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/ARMSSymbol_ttutzf.png",
     franchise: "ARMS",
   },
   {
     id: "77",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/MinecraftSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/MinecraftSymbol_xa4xid.png",
     franchise: "MINECRAFT",
   },
 
   {
     id: "81",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/TekkenSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628122/icons%20smash/TekkenSymbol_j91lhe.png",
     franchise: "Tekken",
   },
   {
     id: "82",
-    background_one: "#232526",
-    background_two: "#414345",
-    icon: Asset.fromModule(
-      require("../assets/icons/franchise_icons/KingdomHeartsSymbol.png")
-    ).uri,
+    background_one: '#232526',
+    background_two: '#414345',
+    icon: "https://res.cloudinary.com/dare4cvkk/image/upload/v1672628121/icons%20smash/KingdomHeartsSymbol_o0aigz.png",
     franchise: "Kingdom Hearts",
   },
 ];
