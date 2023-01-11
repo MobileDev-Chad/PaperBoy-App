878890a9e597ffc24cb97596adaed97c6d3ac483
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-11 00:59:16 -0500

    Added empty cart png

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,9 @@
 export const invincible = require("..//assets/images/invinsible.jpg");
 export const hero = require("../assets/images/hero.jpg");
+export const emptyCart = require("../assets/images/undraw_empty_cart.png");
 
 export default {
-    hero,
-    invincible,
- 
-}
+  hero,
+  invincible,
+  emptyCart,
+};
