ff145864d425720c8d74eaaebe2a45d6c536db17
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-13 17:17:52 -0500

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,5 @@
 export { default as Cart } from "./Cart";
 export { default as WelcomeScreen } from "./WelcomeScreen";
 export { default as HomeScreen } from "./HomeScreen";
-export { default as SearchScreen } from "./SearchScreen";
 export { default as DetailsScreen } from "./DetailsScreen";
 export { default as ContactScreen } from "./ContactScreen";
