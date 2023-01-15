37bfa3830ae65814c4e3dac9677eadfc67cdaa7b
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-14 22:08:32 -0500

    Create styles.js

---
+++ src/constants/styles.js
@@ -0,0 +1,12 @@
+import { Platform } from 'react-native'
+
+import { COLORS, FONTS, SIZES } from './theme'
+
+export default {
+  COLORS,
+  text: {
+    color: COLORS.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+  },
+}
