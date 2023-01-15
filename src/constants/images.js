598b53bffea6c2ca800dc92feeda3f5fd9f072df
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-14 21:51:33 -0500

    Uploaded contact image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
 export const invincible = require("..//assets/images/invinsible.jpg");
 export const hero = require("../assets/images/hero.jpg");
 export const emptyCart = require("../assets/images/undraw_empty_cart.png");
+export const contact = require("../assets/images/contact.png");
 
 export default {
   hero,
   invincible,
   emptyCart,
+  contact
 };
