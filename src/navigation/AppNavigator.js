053dc7fd8c9ca4e548cc08648a3b89d1368f9843
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-14 22:21:26 -0500

    Add Lottie file for app load

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,32 @@
+import React, { useState, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import TabIcon from "../components/TabIcon";
+import ActivityIndicator from "../components/ActivityIndicator";
 import HomeNavigator from "./HomeNavigator";
-import { ContactScreen, SearchScreen, Cart } from "../screens";
+import { ContactScreen, CartScreen } from "../screens";
 import { COLORS } from "../constants";
 
 const Tab = createBottomTabNavigator();
 
-export default AppNavigator = () => {
+export default AppNavigator = ({ navigation }) => {
   const cart = useSelector((state) => state.cart);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setIsLoading(true);
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+      
+    });
+
+    return unsubscribe;
+  }, [navigation]);
 
   const getTotalQuantity = () => {
     let totalQuantity = 0;
@@ -36,54 +53,68 @@ export default AppNavigator = () => {
     },
   };
 
-  return (
-    <Tab.Navigator screenOptions={tabOptions}>
-      <Tab.Screen
-        name="Contact"
-        component={ContactScreen}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabIcon
-              name="email"
-              color={color}
-              type="entypo"
-              focused={focused}
-            />
-          ),
-          headerShown: false,
-        }}
-      />
+  return isLoading ? (
+    <>
+      <ActivityIndicator visible={isLoading} />
+    </>
+  ) : (
+    <>
+      <Tab.Navigator
+        screenOptions={tabOptions}
+        initialRouteName={"Home"}
+      >
+        <Tab.Screen
+          name="Contact"
+          component={ContactScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                name="email"
+                color={color}
+                type="entypo"
+                focused={focused}
+              />
+            ),
+            headerShown: false,
+          }}
+        />
 
-      <Tab.Screen
-        name="Home"
-        component={HomeNavigator}
-        options={({ route }) => ({
-          tabBarIcon: ({ focused, color }) => (
-            <TabIcon name="home" color={color} focused={focused} />
-          ),
-          headerShown: false,
-          tabBarStyle: ((route) => {
-            const routeName = getFocusedRouteNameFromRoute(route) ?? "";
-            console.log(routeName);
-            if (routeName === "Details") {
-              return { display: "none" };
-            }
-            return;
-          })(route),
-        })}
-      />
+        <Tab.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={({ route }) => ({
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                name="home"
+                color={color}
+                type="rounded"
+                focused={focused}
+              />
+            ),
+            headerShown: false,
+            tabBarStyle: ((route) => {
+              const routeName = getFocusedRouteNameFromRoute(route) ?? "";
+              console.log(routeName);
+              if (routeName === "Details") {
+                return { display: "none" };
+              }
+              return;
+            })(route),
+          })}
+        />
 
-      <Tab.Screen
-        name="Cart"
-        component={Cart}
-        options={{
-          tabBarIcon: ({ focused, color }) => (
-            <TabIcon name="shopping-cart" color={color} focused={focused} />
-          ),
-          tabBarBadge: getTotalQuantity() || 0,
-          headerShown: false,
-        }}
-      />
-    </Tab.Navigator>
+        <Tab.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon name="shopping-cart" color={color} focused={focused} />
+            ),
+            tabBarBadge: getTotalQuantity() || 0,
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </>
   );
 };
