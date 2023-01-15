206f54a065b1705a39ca9baa4132cb8d3a135183
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-14 22:09:12 -0500

    Update TabIcon.js

--- src/components/TabIcon.js
+++ src/components/TabIcon.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 import PropTypes from 'prop-types';
+import { COLORS } from '../constants';
 
 export default TabIcon = ({ name, color, style, type }) => {
   
@@ -39,35 +40,35 @@ export default TabIcon = ({ name, color, style, type }) => {
                 color={color}
               />
             );
-          // case 'rounded':
-          //   return (
-          //     <View style={styles.rounded}>
-          //       <EntypoIcon
-          //         style={style ? style : {}}
-          //         name={name}
-          //         size={24}
-          //         color={
-          //           color === COLORS.inactiveGreyColor
-          //             ? COLORS.white
-          //             : color
-          //         }
-          //       />
-          //     </View>
-          //   );
+          case 'rounded':
+            return (
+              <View style={styles.rounded}>
+                <EntypoIcon
+                  style={style ? style : {}}
+                  name={name}
+                  size={24}
+                  color={
+                    color === COLORS.lightGray
+                      ? COLORS.white
+                      : color
+                  }
+                />
+              </View>
+            );
         }
       })()}
     </>
   );
 };
 const styles = StyleSheet.create({
-  // rounded: {
-  //   width: 40,
-  //   height: 40,
-  //   borderRadius: 20,
-  //   backgroundColor: theme.colors.inactiveGreyColor,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
+   rounded: {
+     width: 40,
+     height: 40,
+     borderRadius: 20,
+     backgroundColor: COLORS.lightGray,
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
 });
 
 TabIcon.propTypes = {
