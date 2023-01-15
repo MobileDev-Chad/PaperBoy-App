42a6eeb9a7729d98ab966c26280a7cc0ad4fc6fa
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-14 22:21:40 -0500

    Remove Cart

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
-export { default as Cart } from "./Cart";
 export { default as WelcomeScreen } from "./WelcomeScreen";
+export { default as ContactScreen } from "./ContactScreen";
 export { default as HomeScreen } from "./HomeScreen";
 export { default as DetailsScreen } from "./DetailsScreen";
-export { default as ContactScreen } from "./ContactScreen";
+export { default as CartScreen } from "./CartScreen";
